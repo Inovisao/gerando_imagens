87505006286d2813c367b2e63919884d51cb2069
Roberto Neto <164418185+robertopfneto@users.noreply.github.com>
2024-11-07 18:15:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from process_image import process_and_save_images
+from process_image import process_and_save_images as ps
 
 
 
 number_process_images = int(input("Informe a quantidade de imagens a ser geradas: "))
 image_path = "./input_images" # Defina aqui o caminho das imagens originais
 output_folder = "./output_images" #Defina aqui o caminho de saida
-background_folder = "./background" #Defina aqui o caminho dos fundos das imagens
+# background_folder = "./background" #Defina aqui o caminho dos fundos das imagens
 
 
 
-process_and_save_images(image_path, background_folder, output_folder, number_process_images)
+ps(image_path, output_folder, number_process_images)
