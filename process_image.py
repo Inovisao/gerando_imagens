f853d1ec1ac1d60583769b1caa0a1359aafa27a3
Roberto Neto <164418185+robertopfneto@users.noreply.github.com>
2024-11-08 16:20:11 -0400

    Update process_image.py

--- process_image.py
+++ process_image.py
@@ -3,6 +3,7 @@ import json
 from imgaug import augmenters as iaa
 from PIL import Image
 import numpy as np
+
 def process_and_augment_images(input_folder, output_folder, num_augmentations):
     json_path = os.path.join(input_folder, 'annotations.coco.json')
     
@@ -23,12 +24,15 @@ def process_and_augment_images(input_folder, output_folder, num_augmentations):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     
-    augmenter = iaa.SomeOf((1, 3), [
+    augmenter = iaa.SomeOf((1, 5), [
         iaa.GaussianBlur(sigma=(0, 1.0)),
         iaa.LinearContrast((0.75, 1.5)),
         iaa.AdditiveGaussianNoise(scale=(0, 0.05 * 255)),
         iaa.Multiply((0.8, 1.2)),
-        iaa.Affine(rotate=(-25, 25))
+        iaa.Affine(rotate=[90, 180]),  # Rotação apenas em 90 ou 180 graus
+        iaa.MultiplyBrightness((0.8, 1.2)),  # Ajuste de iluminação
+        iaa.LinearContrast((0.5, 2.0)),  # Ajuste de contraste
+        iaa.AddToHueAndSaturation((-20, 20))  # Ajuste de saturação
     ], random_order=True)
 
     for image_info in coco_data["images"]:
@@ -42,6 +46,8 @@ def process_and_augment_images(input_folder, output_folder, num_augmentations):
         image = Image.open(image_path)
         for i in range(num_augmentations):
             augmented_image = augmenter(image=np.array(image))
+            augmented_image = Image.fromarray(augmented_image)
             output_image_path = os.path.join(output_folder, f"{os.path.splitext(image_info['file_name'])[0]}_aug_{i}.jpg")
-            Image.fromarray(augmented_image).save(output_image_path)
+            augmented_image.save(output_image_path)
             print(f"Imagem aumentada salva em: {output_image_path}")
+
