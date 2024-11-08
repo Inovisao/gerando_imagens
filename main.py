3ce858fa4bca83f1600f57759dd5ccca19fcd2c7
Roberto Neto <164418185+robertopfneto@users.noreply.github.com>
2024-11-08 15:52:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from process_image import process_and_save_images as ps
+import os
+from insering_background import inpainting_with_annotations as inpainting
+from process_image import process_and_augment_images as ps
+
+def create_folder(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+number_per_image = int(input("Informe a quantidade de imagens a ser inseridas por fundo: "))
+number_process_images = int(input("\nInforme a quantidade de imagens a ser geradas por classe: "))
 
 
+# Defina aqui os caminhos da pasta
+background_folder = "./background"
+output_folder = "./final_dataset"
+image_path = "./original_dataset/"
+output_images = "./output_images"
 
-number_process_images = int(input("Informe a quantidade de imagens a ser geradas: "))
-image_path = "./input_images" # Defina aqui o caminho das imagens originais
-output_folder = "./output_images" #Defina aqui o caminho de saida
-# background_folder = "./background" #Defina aqui o caminho dos fundos das imagens
+
+print("\nInserindo imagens em fundos...")
+inpainting(background_folder, image_path, output_images, number_per_image)
+
+print("\nAplicando aumento de dados nas imagens resultantes...")
+ps(output_images, output_folder, number_process_images)
+
+print("\nProcessamento concluído com sucesso!")
 
 
-
-ps(image_path, output_folder, number_process_images)
+#def inpainting_with_annotations(background_folder, original_datasets_folder, output_folder, insects_per_image):
