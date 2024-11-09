367970894f5dc16b9d2cb7f63e23db7baa4ef812
Roberto Neto <164418185+robertopfneto@users.noreply.github.com>
2024-11-09 14:37:56 -0400

    Update create_folder.py

--- create_folder.py
+++ create_folder.py
@@ -3,3 +3,6 @@ import os
 def create_folder(path):
     if not os.path.exists(path):
         os.makedirs(path)
+        print(f"Pasta criada: {path}")
+    else:
+        print(f"Pasta já existe: {path}")
