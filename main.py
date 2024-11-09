4c1e2361eb80059e20a866b605cc65220d6cb974
Roberto Neto <164418185+robertopfneto@users.noreply.github.com>
2024-11-09 14:11:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-import os
+from create_folder import create_folder
 from insering_background import inpainting_with_annotations as inpainting
 from process_image import process_and_augment_images as ps
 
-def create_folder(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
 
 number_per_image = int(input("Informe a quantidade de imagens a ser inseridas por fundo: "))
 number_process_images = int(input("\nInforme a quantidade de imagens a ser geradas por classe: "))
-
 
 # Defina aqui os caminhos da pasta
 background_folder = "./background"
@@ -16,6 +12,11 @@ output_folder = "./final_dataset"
 image_path = "./original_dataset/"
 output_images = "./output_images"
 
+# Cria as pastas se não existirem
+create_folder(background_folder)
+create_folder(output_folder)
+create_folder(image_path)
+create_folder(output_images)
 
 print("\nInserindo imagens em fundos...")
 inpainting(background_folder, image_path, output_images, number_per_image)
@@ -24,4 +25,3 @@ print("\nAplicando aumento de dados nas imagens resultantes...")
 ps(output_images, output_folder, number_process_images)
 
 print("\nProcessamento concluído com sucesso!")
-
